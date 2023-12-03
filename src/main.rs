6e946eeaffaa9b315115721436885556d0b359fc
Yihao Wang <yihao.w@nyu.edu>
2023-12-03 11:37:35 -0500

    add struct

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,47 @@ fn main() {
 
 }
 #[test]
+fn test_tuple_struct() {
+    let mut p = Point(17, 23);
+    p.0 = 2;
+    println!("({}, {})", p.0, p.1);
+}
+
+struct Point(i32, i32);
+
+#[test]
+fn test_named_struct() {
+    let mut peter = Person {
+        name: String::from("Peter"),
+        age: 27,
+    };
+    describe(&peter);
+
+    peter.age = 28;
+    describe(&peter);
+
+    let name = String::from("Avery");
+    let age = 39;
+    let avery = Person { name, age };
+    describe(&avery);
+
+    let jackie = Person {
+        name: String::from("Jackie"),
+        ..avery
+    };
+    describe(&jackie);
+}
+
+struct Person {
+    name: String,
+    age: u8,
+}
+
+fn describe(person: &Person) {
+    println!("{} is {} years old", person.name, person.age);
+}
+
+#[test]
 fn test_exclusive_reference() {
     let mut point = (1, 2);
     let x_coord_ref = &mut point.0;
